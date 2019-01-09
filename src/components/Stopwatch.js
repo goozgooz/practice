6c3153602a66aaa469c5d28cbc39a381cf5427f8
Ashkaan Jaberi <ashkaan14@gmail.com>
2019-01-09 20:41:05 +0000

    stopwatch keeping track of time

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -3,12 +3,33 @@ import React from 'react';
 class Stopwatch extends React.Component {
   state = {
     isRunning: false,
+    elapsedTime: 0,
+    previousTime: 0,
   }
 
   handleStopWatchClick = () => {
-    this.setState({
-      isRunning: !this.state.isRunning
-    })
+    this.setState(prevState => ({
+      isRunning: !prevState.isRunning
+    }));
+      
+  
+    if (!this.state.isRunning) {
+      this.setState({previousTime: Date.now()})
+    }
+  }
+  
+  componentDidMount() {
+    this.intervalID = setInterval (() => this.tick(), 100);
+  }
+  
+  tick = () => {
+    if (this.state.isRunning) {
+      const timeAdded = Date.now() - this.state.previousTime;
+      this.setState(prevState => ({
+        elapsedTime: prevState.elapsedTime + timeAdded,
+        previousTime: Date.now(),
+      }))
+    }
   }
 
   render(){
