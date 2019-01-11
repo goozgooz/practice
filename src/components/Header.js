076ace8f9cf37e9f434f59771882c6efc241ebf4
Ashkaan Jaberi <ashkaan14@gmail.com>
2019-01-11 17:15:46 +0000

    checking prop types

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Stats from './Stats';
 import Stopwatch from './Stopwatch';
 
@@ -11,6 +13,11 @@ const Header = (props) => {
       <Stopwatch />
     </header>
   );
+}
+
+Header.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string,
 }
 
 export default Header;
